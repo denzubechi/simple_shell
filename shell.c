64824dc932d09757aa4162afb0d8e55d04e8c56c
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-08-14 18:17:28 -0700

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,150 +1,87 @@
 #include "holberton.h"
 
 /**
- * shell - simple shell
- * @build: input build
+ * execute - executes the command
+ * @cmd: command to run
+ * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
  */
-void shell(config *build)
+int execute(char **cmd)
 {
-	while (true)
-	{
-		checkAndGetLine(build);
-		if (splitString(build) == false)
-			continue;
-		if (findBuiltIns(build) == true)
-			continue;
-		checkPath(build);
-		forkAndExecute(build);
-	}
-}
 
-/**
- * checkAndGetLine - check stdin and retrieves next line; handles
- * prompt display
- * @build: input build
- */
-void checkAndGetLine(config *build)
-{
-	register int len;
-	size_t bufferSize = 0;
-	char *ptr, *ptr2;
-
-	build->args = NULL;
-	build->envList = NULL;
-	build->lineCounter++;
-	if (isatty(STDIN_FILENO))
-		displayPrompt();
-	len = getline(&build->buffer, &bufferSize, stdin);
-	if (len == EOF)
-	{
-		freeMembers(build);
-		if (isatty(STDIN_FILENO))
-			displayNewLine();
-		if (build->errorStatus)
-			exit(build->errorStatus);
-		exit(EXIT_SUCCESS);
-
-	}
-	ptr = _strchr(build->buffer, '\n');
-	ptr2 = _strchr(build->buffer, '\t');
-	if (ptr || ptr2)
-		insertNullByte(build->buffer, len - 1);
-	stripComments(build->buffer);
-}
-
-/**
- * stripComments - remove comments from input string
- * @str: input string
- * Return: length of remaining string
- */
-void stripComments(char *str)
-{
-	register int i = 0;
-	_Bool notFirst = false;
-
-	while (str[i])
-	{
-		if (i == 0 && str[i] == '#')
-		{
-			insertNullByte(str, i);
-			return;
-		}
-		if (notFirst)
-		{
-			if (str[i] == '#' && str[i - 1] == ' ')
-			{
-				insertNullByte(str, i);
-				return;
-			}
-		}
-		i++;
-		notFirst = true;
-	}
-}
-
-/**
- * forkAndExecute - fork current build and execute processes
- * @build: input build
- */
-void forkAndExecute(config *build)
-{
+	pid_t child_pid;
 	int status;
-	pid_t f1 = fork();
 
-	convertLLtoArr(build);
-	if (f1 == -1)
+	if (strncmp("exit", cmd[0], 4) == 0)
+		return (-1);
+
+	child_pid = fork();
+
+	if (child_pid == -1)
 	{
-		perror("error\n");
-		freeMembers(build);
-		freeArgs(build->envList);
-		exit(1);
+		perror("Error");
+		return (1);
 	}
-	if (f1 == 0)
+	else if (child_pid == 0)
 	{
-		if (execve(build->fullPath, build->args, build->envList) == -1)
+		if (execve(cmd[0], cmd, NULL) == -1)
 		{
-			errorHandler(build);
-			freeMembers(build);
-			freeArgs(build->envList);
-			if (errno == ENOENT)
-				exit(127);
-			if (errno == EACCES)
-				exit(126);
+			perror("Error");
+			exit(-1);
 		}
-	} else
-	{
-		wait(&status);
-		if (WIFEXITED(status))
-			build->errorStatus = WEXITSTATUS(status);
-		freeArgsAndBuffer(build);
-		freeArgs(build->envList);
 	}
+	else
+		wait(&status);
+
+	return (0);
 }
 
-/**
- * convertLLtoArr - convert linked list to array
- * @build: input build
- */
-void convertLLtoArr(config *build)
-{
-	register int i = 0;
-	size_t count = 0;
-	char **envList = NULL;
-	linked_l *tmp = build->env;
 
-	count = list_len(build->env);
-	envList = malloc(sizeof(char *) * (count + 1));
-	if (!envList)
+/**
+ * main - main simple shell
+ * @argc: number of arguments
+ * @argv: list of command line arguments
+ * Return: Always 0, -1 on error.
+ */
+
+int main(int argc, char **argv)
+{
+
+	int response;
+	char **tokens;
+	size_t bufsize = BUFSIZ;
+	int isPipe = 0;
+	char *buffer;
+
+	if (argc >= 2)
 	{
-		perror("Malloc failed\n");
+		/*TODO: Handle cases where there is no argument, only the command*/
+		if (execve(argv[1], argv, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+		return (0);
+	}
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
 		exit(1);
 	}
-	while (tmp)
-	{
-		envList[i] = _strdup(tmp->string);
-		tmp = tmp->next;
-		i++;
-	}
-	envList[i] = NULL;
-	build->envList = envList;
+
+	do {
+		if (isatty(fileno(stdin)))
+		{
+			isPipe = 1;
+			_puts("cisfun#: ");
+		}
+
+		getline(&buffer, &bufsize, stdin);
+		buffer[_strlen(buffer) - 1] = '\0';
+		tokens = stringToTokens(buffer);
+		response = execute(tokens);
+	} while (isPipe && response != -1);
+
+	return (0);
 }
